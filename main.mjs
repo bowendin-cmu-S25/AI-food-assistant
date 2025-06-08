3c94ae49f8611979d8fe3ec5ee51b9a180a70d2b
oliver408i <rufanyin29a@gmail.com>
2025-06-08 11:58:02 -0700

    ejs setup

--- main.mjs
+++ main.mjs
@@ -1,6 +1,15 @@
 import express from 'express';
+import { mongoose } from 'mongoose';
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
